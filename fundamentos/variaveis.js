29a9152206254b96f2e43fd68e29ce82f09fde6d
Bruno Henrique <brunodoreadelima@outlook.com>
2024-09-13 21:33:06 -0300

    estudo das variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * Estudo de constante e variáveis
+ * @author Bruno Henrique
+ */
+
+console.clear()
+
+// variáveis 
+let nome 
+let idade 
+let vip
+
+nome = "Bruno Henrique"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = "27"
+console.log(typeof(idade))
+console.log(idade) 
+
+vip = true 
+console.log(typeof(vip))
+console.log(vip) 
