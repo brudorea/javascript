606e7c97093e5d6848fd7461c7f3364f49be1246
Bruno Henrique <brunodoreadelima@outlook.com>
2024-09-19 22:16:17 -0300

    ficha do aluno

---
+++ exercícios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de um aluno de academia 
+ * @author Bruno Henrique 
+ */
+
+// variáveis
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+nome = "Bruno Henrique"
+idade = 27
+peso = 69
+altura = 1.73
+vip = true
+
+// saída
+console.log("Ficha do aluno")
+console.log("___________________________")
+console.log(`Nome : ${nome}`)
+console.log(`Idade : ${idade}`)
+console.log(`Peso : ${peso}`)
+console.log(`Altura : ${altura}`)
+console.log(`VIP : ${vip}`)
+
+
