789841f1bb33b763f3e5f8e17e7e7a1fdff0b8b9
Bruno Henrique <brunodoreadelima@outlook.com>
2024-09-24 22:22:14 -0300

    Atualização e inclusão de novas etapas

--- exercícios/academia.js
+++ exercícios/academia.js
@@ -9,13 +9,21 @@ let idade
 let peso
 let altura
 let vip
+let fcm
+let imc
 
 console.clear()
+
+// entrada de dados
 nome = "Bruno Henrique"
 idade = 27
 peso = 69
 altura = 1.73
 vip = true
+
+//processamento
+fcm = 208 - (0.7 * idade)
+imc = peso / (altura * altura)
 
 // saída
 console.log("Ficha do aluno")
@@ -25,5 +33,7 @@ console.log(`Idade : ${idade}`)
 console.log(`Peso : ${peso}`)
 console.log(`Altura : ${altura}`)
 console.log(`VIP : ${vip}`)
+console.log(`FCM: ${fcm}`)
+console.log(`IMC: ${imc.toFixed(2)}`)
 
 
---
+++ fundamentos/operadores.js
@@ -0,0 +1,17 @@
+/**
+ * Operadores aritméticos
+ * @author Bruno Henrique 
+ */
+
+console.clear()
+
+console.log("Operador       | Simbologia")
+console.log("Atribuição     |          =")
+console.log("Soma           |          +")
+console.log("Subtração      |          -")
+console.log("Multiplicação  |          *")
+console.log("Divisão        |          /")
+console.log("Módulo         |          % (resto da divisão)")
+
+
+
