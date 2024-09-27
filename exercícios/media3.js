855969498b8d0319e12dee65f8d84b52f6a92d54
Bruno Henrique <brunodoreadelima@outlook.com>
2024-09-27 20:11:53 -0300

    calculo da media

---
+++ exercícios/media3.js
@@ -0,0 +1,27 @@
+/**
+ * Calculo da média aritmética de 3 valores
+ * @author Bruno Henrique
+ */
+
+// importar a biblioteca read-linesync
+const input = require('readline-sync')
+
+// variáveis
+let num1, num2, num3, media
+
+console.clear()
+console.log("Cálculo da média aritmética de 3 numeros")
+
+// entrada 
+// No console, entradas(input) númericas precisam ser convertidas com uso do método Number() 
+num1 = Number (input.question("Digite o primeiro numero: "))
+num2 = Number (input.question("Digite o segundo numero: "))
+num3 = Number (input.question("Digite o terceiro numero: "))
+
+// processamento
+media = (num1 + num2 + num3) / 3
+
+// saída 
+console.log(`Média: ${media.toFixed(1)}`)
+
+
