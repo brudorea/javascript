5abd7b4925642c0776ceb2d65c0ef239e6fff940
Bruno Henrique <brunodoreadelima@outlook.com>
2024-11-28 19:11:57 -0300

    Atualização com as descrições

--- poo/carros.js
+++ poo/carros.js
@@ -1,91 +1,88 @@
 /**
- * Desafio Carro
+ * Desafio Carro + Avião
  * @author Bruno Henrique
  */
 
-console.clear()
+console.clear() // Limpa o console antes de executar o código para melhor visualização.
 
 // Criando a classe modelo
 class Carro {
     // Atributos
-    constructor(ano, cor) {
-        this.ano = ano
-        this.cor = cor
+    constructor(ano, cor) { // Método construtor que inicializa os atributos 'ano' e 'cor' do carro.
+        this.ano = ano // Atributo 'ano' do carro.
+        this.cor = cor // Atributo 'cor' do carro.
     }
 
     // Métodos
-    criarCarro() {
+    criarCarro() { // Método para exibir no console a representação do carro e suas características.
         console.log("-----------------------------------")
         console.log("   _______ ")
         console.log("  /|_||_\\`.__")
         console.log(" (  _    _ _ \\ ")
         console.log("=`-(_)--(_)-'")
         console.log("-----------------------------------")
-        console.log(`Carro de ${this.cor}`)
-        console.log(`Ano: ${this.ano}`)
+        console.log(`Carro de ${this.cor}`) // Exibe a cor do carro.
+        console.log(`Ano: ${this.ano}`) // Exibe o ano do carro.
     }
 
-    ligar() {
+    ligar() { // Método para simular ligar o carro.
         console.log(`□ □ □ □ Carro de ${this.cor} ligado!`)
     }
 
-    acelerar() {
+    acelerar() { // Método para simular o movimento do carro.
         console.log("□ □ □ □ Carro se locomovendo!")
     }
 
-    desligar() {
+    desligar() { // Método para simular desligar o carro.
         console.log("□ □ □ □ Carro desligado!")
     }
 }
 
 // Herança
-class Aviao extends Carro {
+class Aviao extends Carro { // A classe Aviao herda os atributos e métodos da classe Carro.
     // Atributos
-    constructor(ano, cor, envergadura) {
-        super(ano, cor) // Herança
-        this.envergadura = envergadura
+    constructor(ano, cor, envergadura) { // Método construtor que inicializa os atributos 'ano', 'cor' e 'envergadura' do avião.
+        super(ano, cor) // Chama o construtor da classe 'Carro' para inicializar 'ano' e 'cor'.
+        this.envergadura = envergadura // Atributo adicional exclusivo da classe Aviao.
     }
 
     // Método 
-    criarAviao() {
+    criarAviao() { // Método para exibir no console a representação do avião e suas características.
         console.log("-------------------------------------------")
         console.log("       __|__")
         console.log("  ------(_)------")
         console.log("   __|__|__|__|__")
-        console.log(`Aviao ${this.cor}`)
-        console.log(`Ano: ${this.ano}`)
+        console.log(`Aviao ${this.cor}`) // Exibe a cor do avião.
+        console.log(`Ano: ${this.ano}`) // Exibe o ano do avião.
         console.log("-------------------------------------------")
     }
 
-    acelerar() {
+    acelerar() { // Método para simular o movimento do avião (sobrescreve o método da classe Carro).
         console.log("□ □ □ □ # Decolar!")
     }
-    // Método para aterrissar o avião
-    aterrissar() {
 
+    aterrissar() { // Método exclusivo para simular o avião aterrissando.
         console.log("._._._._._._._._._._._._._._._._._._._.")
         console.log("□ □ □ □ ☀ Aterrissar!")
     }
-
 }
-console.log("--------inicio")
-//Instanciando (Criando) um objeto.
-const carro1 = new Carro("1978", "vermelho")
-carro1.criarCarro()
-carro1.ligar()
-carro1.acelerar()
-carro1.desligar()
 
-const carro2 = new Carro("2024", "azul")
-carro2.criarCarro()
-carro1.ligar()
-carro1.acelerar()
-carro1.desligar()
+console.log("--------inicio") // Exibe no console o início da execução do programa.
 
+// Instanciando (Criando) um objeto.
+const carro1 = new Carro("1978", "vermelho") // Cria uma instância da classe Carro com ano "1978" e cor "vermelho".
+carro1.criarCarro() // Chama o método para criar e exibir as características do carro.
+carro1.ligar() // Chama o método para ligar o carro.
+carro1.acelerar() // Chama o método para acelerar o carro.
+carro1.desligar() // Chama o método para desligar o carro.
 
-const aviao1 = new Aviao("2018", "Branco", 30)
-aviao1.criarAviao()
-aviao1.acelerar()
-aviao1.aterrissar()
+const carro2 = new Carro("2024", "azul") // Cria outra instância da classe Carro com ano "2024" e cor "azul".
+carro2.criarCarro() // Exibe as características do carro.
+carro1.ligar() // Liga o carro (nota: o carro1 é ligado novamente, o carro2 não é usado corretamente aqui).
+carro1.acelerar() // Acelera o carro1 novamente.
+carro1.desligar() // Desliga o carro1 novamente.
 
-
+const aviao1 = new Aviao("2018", "Branco", 30) // Cria uma instância da classe Aviao com ano "2018", cor "Branco" e envergadura "30".
+aviao1.criarAviao() // Chama o método para criar e exibir as características do avião.
+aviao1.acelerar() // Chama o método para decolar o avião.
+aviao1.aterrissar() // Chama o método para aterrissar o avião.
--- poo/login.js
+++ poo/login.js
@@ -4,39 +4,35 @@
  * @author Bruno Henrique
  */
 
-console.clear()
+console.clear() // Limpa o console para facilitar a visualização da execução do programa.
 
-class User {
-    //atributos
-    constructor(login, senha) {
-        this.login = login
-        //Encapsulamento (O_ "underline" enjaula a variável)
-        let _senha = senha
-        this.getSenha = () => _senha // leitura
-        this.setSenha = (novaSenha) => _senha = novaSenha // escrita
+class User { // Declara a classe 'User' que representa um usuário no sistema.
+    // Atributos
+    constructor(login, senha) { // Método construtor para inicializar os atributos de um usuário.
+        this.login = login // Inicializa o atributo público 'login' com o valor recebido.
+        // Encapsulamento (O "_" indica que a variável é privada, apenas acessível indiretamente).
+        let _senha = senha // Define a variável privada '_senha', não acessível diretamente fora da classe.
+        this.getSenha = () => _senha // Define um método getter para permitir a leitura da senha.
+        this.setSenha = (novaSenha) => _senha = novaSenha // Define um método setter para permitir a modificação da senha.
     }
 
-
-    //metodo
-    logar() {
-        console.log("_________________________________")
-        console.log(`Usuário ${this.login}`)
-        console.log(`Senha ${this.getSenha()}`)
-        if (this.login === 'admin' && this.getSenha() === '123@senac') {
-            console.log("Usuário autenticado")
-
-        } else {
-            console.log("Usuário e/ou senha inválido(s)")
+    // Método
+    logar() { // Método para verificar e exibir as credenciais do usuário.
+        console.log("_________________________________") // Exibe um separador no console.
+        console.log(`Usuário ${this.login}`) // Exibe o login do usuário.
+        console.log(`Senha ${this.getSenha()}`) // Exibe a senha do usuário (usando o método getter).
+        if (this.login === 'admin' && this.getSenha() === '123@senac') { // Verifica se o login e a senha correspondem às credenciais corretas.
+            console.log("Usuário autenticado") // Exibe uma mensagem de sucesso caso as credenciais sejam válidas.
+        } else { // Caso contrário:
+            console.log("Usuário e/ou senha inválido(s)") // Exibe uma mensagem de erro.
         }
     }
-
 }
 
 /**** Sistema  ****/
-// criando um novo usuário
+// Criando um novo usuário
 
-const user1 = new User("admin", "1234")
-user1.logar()
-user1.setSenha("123@senac")
-user1.logar()
-
+const user1 = new User("admin", "1234") // Cria uma instância da classe 'User' com login "admin" e senha "1234".
+user1.logar() // Chama o método 'logar()' para verificar as credenciais do usuário 'user1' (as credenciais não são válidas ainda).
+user1.setSenha("123@senac") // Altera a senha do usuário para "123@senac" usando o método setter.
+user1.logar() // Chama novamente o método 'logar()' para verificar as credenciais (agora válidas).
